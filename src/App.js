d27437e0743a63c28adc3744854bf96133b66bc3
agata okolska <agata.okolska@gmail.com>
2018-07-23 12:16:35 +0200

    change names of tags - embeding expressions

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const appClasPrefix = 'App'
 const App = () => (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className={`${appClasPrefix}`}>
+        <header className={`${appClasPrefix}-header`}>
+          <img src={logo} className={`${appClasPrefix}-logo`} alt={'logo'} />
+          <h1 className={`${appClasPrefix}-title`}>Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className={`${appClasPrefix}-intro`}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
