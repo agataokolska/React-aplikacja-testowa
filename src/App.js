389fc8fd6ff483fe55f3eff79e0c53ccfd8ed355
agata okolska <agata.okolska@gmail.com>
2018-07-23 12:19:32 +0200

    delete app css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-const appClasPrefix = 'App'
 const App = () => (
-      <div className={`${appClasPrefix}`}>
-        <header className={`${appClasPrefix}-header`}>
-          <img src={logo} className={`${appClasPrefix}-logo`} alt={'logo'} />
-          <h1 className={`${appClasPrefix}-title`}>Welcome to React</h1>
-        </header>
-        <p className={`${appClasPrefix}-intro`}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+     <div>
+
       </div>
     )
 
