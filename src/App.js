0a5f22322ad1192a578808c7ebcd18d7f5621d0e
agata okolska <agata.okolska@gmail.com>
2018-07-24 14:54:51 +0200

    move drawer to new component -Sidebar

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route} from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
-import Drawer from 'material-ui/Drawer'
-import MenuItem from 'material-ui/MenuItem'
 
 import BasicComponents from './BasicComponents'
 import Dashboard from './Dashboard'
 import PassingParameters from './PassingParameters'
-
+import Sidebar from './Sidebar';
 
 const App = () => (
   <div>
     <Router>
       <div>
         <AppBar
-        title={'JFDDL5 APP'}
+          title={'JFDDL5 APP'}
         />
-        <Drawer
-        open={true}
-        >
-            <MenuItem>Menu item 1</MenuItem>
-        </Drawer>
-        <Link to={'/dashboard'}>Dashboard</Link>
-        <Link to={'/basiccomponents'}>BasicComponents</Link>
-        <Link to={'/passing-parameters/cos'}>PassingParameters</Link>
+        <Sidebar/>
+
 
         <Route path={'/'} exact component={Dashboard} />
         <Route path={'/dashboard'} component={Dashboard} />
---
+++ src/Sidebar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+
+const Sidebar = () => (
+    <div>
+        <Drawer
+            open={true}
+        >
+            <Link
+                to={'/dashboard'}
+                style={{ textDecoration: 'none' }}
+            >
+                <MenuItem>
+                    Dashboard
+        </MenuItem>
+            </Link>
+
+            <Link
+                to={'/basiccomponents'}
+                style={{ textDecoration: 'none' }}
+            >
+                <MenuItem>
+                    BasicComponents
+        </MenuItem>
+            </Link>
+
+            <Link
+                to={'/passing-parameters/cos'}
+                style={{ textDecoration: 'none' }}
+            >
+                <MenuItem>
+                    PassingParameters
+        </MenuItem>
+            </Link>
+        </Drawer>
+    </div>
+)
+
+export default Sidebar
