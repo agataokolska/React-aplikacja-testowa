f954e03e3be903fbdd9745025eb6f91921dce8ca
agata okolska <agata.okolska@gmail.com>
2018-07-24 10:40:41 +0200

    passing props

--- src/App.js
+++ src/App.js
@@ -2,22 +2,20 @@ import React from 'react'
 
 import Border from './Border'
 import SingingButton from './SingingButton'
+import SingingButtonWithBorder from './SingingButtonWithBorder';
 
 const App = () => (
-     
-       <Border>
-         <SingingButton
-         label={'Przycisk 1'}
-         sound={'La la la'}
-         makeSound={()=> alert('Im from makeSound')}
-         />
-         <SingingButton
-         label={'Przycisk 2'}
-         sound={'Kotki dwa!'}
-         />
 
-       </Border>
-      
-    )
+  <Border>
+    <SingingButtonWithBorder
+      label={'Przycisk 1'}
+      sound={'La la la'}
+      makeSound={() => alert('Im from makeSound')}
+    />
+
+
+  </Border>
+
+)
 
 export default App
---
+++ src/SingingButtonWithBorder.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Border from './Border'
+import SingingButton from './SingingButton'
+
+const SingingButtonWithBorder = (props) => (
+    <Border>
+        <SingingButton
+        label={props.label}
+        sound={props.sound}
+        makeSound={props.makeSound}
+         />
+    </Border>
+)
+
+export default SingingButtonWithBorder
