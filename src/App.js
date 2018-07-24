f37d6c1aa2cfd163926569dc61be8baba4dd3e57
agata okolska <agata.okolska@gmail.com>
2018-07-24 12:05:49 +0200

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import BasicComponents from './BasicComponents'
+import Dashboard from './Dashboard'
+
 
 const App = () => (
-<BasicComponents/>
+<div>
+<Router>
+    <Route path={'/dashboard'} component={Dashboard}/>
+</Router>
+</div>
 )
 
 export default App
---
+++ src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Dashboard = () => (
+    <div>
+        <h1>Hello in JFDDL app!</h1>
+
+    </div>
+)
+
+export default Dashboard
