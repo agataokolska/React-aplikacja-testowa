8c20642ce2a5fb45bcc9fdd2ea71974ffedff9f5
agata okolska <agata.okolska@gmail.com>
2018-07-24 14:35:55 +0200

    add drawer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 
 import BasicComponents from './BasicComponents'
 import Dashboard from './Dashboard'
@@ -14,6 +16,11 @@ const App = () => (
         <AppBar
         title={'JFDDL5 APP'}
         />
+        <Drawer
+        open={true}
+        >
+            <MenuItem>Menu item 1</MenuItem>
+        </Drawer>
         <Link to={'/dashboard'}>Dashboard</Link>
         <Link to={'/basiccomponents'}>BasicComponents</Link>
         <Link to={'/passing-parameters/cos'}>PassingParameters</Link>
