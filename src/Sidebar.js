34867b244751f45459a373dfd9ead897693c2262
agata okolska <agata.okolska@gmail.com>
2018-07-24 16:49:17 +0200

    update Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
 
