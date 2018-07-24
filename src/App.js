625d552d7486f9d99ca0c3445f3e347bad173898
agata okolska <agata.okolska@gmail.com>
2018-07-24 13:49:58 +0200

    passing parameters- link

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 import BasicComponents from './BasicComponents'
 import Dashboard from './Dashboard'
-
+import PassingParameters from './PassingParameters'
 
 const App = () => (
-<div>
-<Router>
   <div>
-    <Link to={'/dashboard'}>Dashboard</Link>
-    <Link to={'/basiccomponents'}>BasicComponents</Link>
-    <Route path={'/'} exact component={Dashboard}/>
-    <Route path={'/dashboard'} component={Dashboard}/>
-    <Route path={'/basiccomponents'} component={BasicComponents}/>
+    <Router>
+      <div>
+        <Link to={'/dashboard'}>Dashboard</Link>
+        <Link to={'/basiccomponents'}>BasicComponents</Link>
+        <Link to={'/passing-parameters/cos'}>PassingParameters</Link>
+
+        <Route path={'/'} exact component={Dashboard} />
+        <Route path={'/dashboard'} component={Dashboard} />
+        <Route path={'/basiccomponents'} component={BasicComponents} />
+        <Route path={'/passing-parameters/:parameterName'} component={PassingParameters} />
+      </div>
+    </Router>
   </div>
-</Router>
-</div>
 )
 
 export default App
