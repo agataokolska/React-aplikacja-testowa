bf08a30a8dc5b64064dcc113ff6b3b4e6c717b40
agata okolska <agata.okolska@gmail.com>
2018-07-25 10:06:46 +0200

    handler not working

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,30 @@
 import React from 'react'
 
 class Counter extends React.Component {
-   
-        state = {
+
+    state = {
         number: 0
-        }
-    
-    render (){
-       return(
-           <div>
-               <h1>{this.state.number}</h1>   
-               <button>+</button>
-               <button>-</button>
-
-            </div>
-       ) 
     }
-}
 
-export default Counter
+
+    incHandler = () => {
+        this.state.number = this.state.number + 1
+    }
+
+        render(){
+            return (
+                <div>
+                    <h1>{this.state.number}</h1>
+                    <button
+                    onClick={this.incHandler}
+                    >
+                        +
+                    </button>
+                    <button>-</button>
+
+                </div>
+            )
+        }
+    }
+
+    export default Counter
