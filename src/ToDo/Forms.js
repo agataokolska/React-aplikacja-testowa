43bd7a62292f27dfe94f6e7126a6b9107bc57427
agata okolska <agata.okolska@gmail.com>
2018-07-25 14:35:31 +0200

    add textField and RaisedButton with full width

--- src/ToDo/Forms.js
+++ src/ToDo/Forms.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import PaperRefined from '../components/PaperRefined';
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 const Forms = (props) => (
 
     <PaperRefined>
-            <input 
+            <TextField 
             type={'text'}
+            fullWidth={true}
             value={props.newTaskText}
             onChange={props.onNewTaskTextChanged}
             />
-            <button>Dodaj</button>
+            <RaisedButton label="Dodaj" primary={true}
+            fullWidth={true} />
     </PaperRefined>
 )
 
