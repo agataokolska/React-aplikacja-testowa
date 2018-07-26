9e1239ff406284af9dcfad71d3ab70776022a2b8
agata okolska <agata.okolska@gmail.com>
2018-07-26 10:25:11 +0200

    add local storage to counter

--- src/Counter.js
+++ src/Counter.js
@@ -6,6 +6,19 @@ class Counter extends React.Component {
         number: this.props.number
     }
 
+
+    componentDidMount(){
+        const lastState = JSON.parse(localStorage.getItem('jfddl-app-counter-state'))
+
+        if (lastState===null) return
+
+        this.setState(lastState)
+
+    }
+
+    componentWillUnmount(){
+        localStorage.setItem('jfddl-app-counter-state', JSON.stringify(this.state))
+    }
     incHandler = () => {
         this.setState({
             number: this.state.number + 1
